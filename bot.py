ec349cb359a4931f2bfd0d9b9765ceab72dd357d
RusslSS <r.s.stancek@gmail.com>
2023-12-29 18:17:44 +0100

    newsteller, selfunverify, list time untill unban, listpermissions

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ from os import getenv
 from dotenv import load_dotenv
 from random import randint, choice
 from datetime import datetime, timedelta
-from interactions import (Client, Intents, Status, Permissions, SlashContext, Member, Task, IntervalTrigger, OptionType, Activity, ActivityType, 
-                          listen, slash_command, slash_option, slash_default_member_permission)
+from interactions import (Client, Intents, Status, SlashContext, Member, Task, IntervalTrigger, OptionType, Activity, ActivityType, 
+                          listen, slash_command, slash_option)
 from interactions.api.events import MessageCreate
 
 # Custom lib
@@ -46,7 +46,6 @@ async def ban(bannee: User):
     await bannee.instance.remove_role(NORMAL_ROLE)
     BAN_LIST.pop(BAN_LIST.index(bannee))
     BANNED.append(bannee)
-    return f'Banned {bannee.instance.display_name} until {bannee.end_date}'
 
 async def check_unban():
     unbanned = []
@@ -59,7 +58,6 @@ async def check_unban():
         BANNED.pop(i)
 
 @slash_command(name="gasparko", description="Better hope it's not cold outside")
-@slash_default_member_permission(Permissions.VIEW_CHANNEL)
 async def gasparko(ctx: SlashContext):
     value = randint(-20, 100)
     emote = ""
@@ -79,8 +77,9 @@ async def gasparko(ctx: SlashContext):
         emote = '<:klasika:1070481083084328970>'
     await ctx.send(f'{value}cm {emote}')
 
-@slash_command(name="online", description="Get information about online users")
-@slash_default_member_permission(Permissions.MANAGE_EMOJIS_AND_STICKERS)
+# ADMIN COMMANDS #
+##########################################################################################
+@slash_command(name="online", description="**ADMIN**: Get information about online users")
 async def online(ctx: SlashContext):
     await ctx.guild.gateway_chunk()
     reply = ""
@@ -89,10 +88,20 @@ async def online(ctx: SlashContext):
     reply += f'Total: {len([m for m in ctx.guild.humans if m.status and m.status != Status.OFFLINE])}'
     await ctx.send(reply, ephemeral=True)
 
+@slash_command(name="list_permissions", description="**ADMIN**: List permissions for each of user's roles")
+@slash_option(name="user", description="User to check", required=True, opt_type=OptionType.USER)
+async def list_permissions(ctx: SlashContext, user: Member):
+    await ctx.guild.gateway_chunk()
+    roles = user.roles
+    message = ""
+    for role in roles:
+        message += f'{role.name}:\n\t{[permission.name for permission in role.permissions]}\n'
+    await ctx.send(message, ephemeral=True)
+##########################################################################################
+
 @slash_command(name="voteban", description="Vote to ban user")
 @slash_option(name="naughty_boy", description="Someone about to get banned", required=True, opt_type=OptionType.USER)
 @slash_option(name="hours", description="Time in hours", required=True, opt_type=OptionType.INTEGER, min_value=1, max_value=24)
-@slash_default_member_permission(Permissions.VIEW_CHANNEL)
 async def voteban(ctx: SlashContext, naughty_boy: Member, hours: int):
     await ctx.guild.gateway_chunk()
     active_users = len([m for m in ctx.guild.humans if m.status and m.status != Status.OFFLINE])
@@ -126,7 +135,8 @@ async def voteban(ctx: SlashContext, naughty_boy: Member, hours: int):
 
     curr_user = get_user(naughty_boy.id, BAN_LIST)
     if curr_user.is_bannable():
-        message = await ban(curr_user)
+        await ban(curr_user)
+        message = f'Banned {curr_user.instance.display_name} until {curr_user.end_date}'
         await ctx.guild.get_channel(VAZENIE_ROOM).send(message)
         await ctx.guild.get_channel(ctx.channel_id).send(message)
 
@@ -135,23 +145,39 @@ async def voteban(ctx: SlashContext, naughty_boy: Member, hours: int):
 # @interactions.slash_command(name="voteunban", description=)
 # TODO check if user can unvote by checking if he is in the voter list
 
+@slash_command(name="self_unverify", description="For nerds; ban yourself for a given time period")
+@slash_option(name="hours", description="Time in hours", required=True, opt_type=OptionType.INTEGER, min_value=1, max_value=24)
+async def self_unverify(ctx: SlashContext, hours: int):
+    BAN_LIST.append(User(ctx.member.id, ctx.member.display_name, ctx.member.id, hours, 0, ctx.member))
+    curr_user = get_user(ctx.member.id, BAN_LIST)
+    await ban(curr_user)
+    message = f'{curr_user.instance.display_name} self unverified until {curr_user.end_date}'
+    await ctx.guild.get_channel(VAZENIE_ROOM).send(message)
+    await ctx.guild.get_channel(ctx.channel_id).send(message)
+
+@slash_command(name="time_left", description="How long until I get unbanned?")
+async def time_left(ctx: SlashContext):
+    await ctx.send(f'You will be banned until {get_user(ctx.member.id, BANNED).end_date}', ephemeral=True)
 
 @slash_command(name="voteban_stats", description="How many votes does each bannee have?")
-@slash_default_member_permission(Permissions.VIEW_CHANNEL)
-async def votebanstats(ctx: SlashContext):
+async def voteban_stats(ctx: SlashContext):
     if BAN_LIST:
         message = print_stats(BAN_LIST)
     else:
         message = "There currently are no votes"
     await ctx.send(message)
 
+# NEWS TELLER #
+###########################################################################
 @slash_command(name="whats_new", description="What are some new features?")
-@slash_default_member_permission(Permissions.VIEW_CHANNEL)
 async def whats_new(ctx: SlashContext):
     message = "DEEZ NUTS... but now for real:\
         \r\t1) **/whats_new** - describes some new features, get more detail by invoking the **/whats_new** command || fuck recursion <:pocem:1037501105774538863> ||\
-        \r\t2) **/voteban_stats** - shows current vote count for each ban candidate"
-    await ctx.send(message, ephemeral=True)
+        \r\t2) **/voteban_stats** - shows current vote count for each ban candidate\
+        \r\t3) **/self_unverify** - bans you for a given time period set by you\
+        \r\t4) **/time_left** - shows when you will be unbanned, you need to be banned to see that tho"
+    await ctx.send(message)
+###########################################################################
 
 @listen()
 async def on_message_create(event: MessageCreate):
